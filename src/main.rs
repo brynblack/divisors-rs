df66378d2617434bbc67d9c979def89e70e5241f
brynblack <49110391+brynblack@users.noreply.github.com>
2022-05-08 19:07:18 +1000

    Type annotated vector

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ impl Config {
 }
 
 fn trial_division(n: u64) -> Vec<u64> {
-    let mut divisors = Vec::new();
+    let mut divisors: Vec<u64> = Vec::new();
     let limit = (n as f64).sqrt() as u64;
     for i in 1..=limit {
         if n % i == 0 {
