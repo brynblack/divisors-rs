66a1a3f67c8948b4db65b6be71225a71d52ac180
brynblack <49110391+brynblack@users.noreply.github.com>
2022-05-12 18:03:30 +1000

    Implemented panic-less solution for error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::num;
 
 pub struct Config {
@@ -5,25 +6,26 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(args: &[String]) -> Result<Config, Box<dyn Error>> {
+        // Check if an insufficient number of arguments were provided
         if args.len() < 2 {
-            return Err("Usage: divisors <integral>");
+            return Err("Usage: divisors <integral>".into());
         }
+
+        // Parse the 2nd argument to an integral
         let integral: u64 = match args[1].parse() {
             Ok(num) => num,
-            Err(err) => match err.kind() {
-                num::IntErrorKind::InvalidDigit => {
-                    return Err("Argument provided is not a positive integral");
-                }
-                num::IntErrorKind::PosOverflow => {
-                    return Err("Integral provided is too large");
-                }
-                other_err => {
-                    // TODO: Replace with "return Err(...)" somehow
-                    panic!("{:?}", other_err);
-                }
-            },
+            Err(err) => {
+                return match err.kind() {
+                    num::IntErrorKind::InvalidDigit => {
+                        Err("Argument provided is not a positive integral".into())
+                    }
+                    num::IntErrorKind::PosOverflow => Err("Integral provided is too large".into()),
+                    _ => Err(err.into()),
+                };
+            }
         };
+
         Ok(Config { integral })
     }
 }
